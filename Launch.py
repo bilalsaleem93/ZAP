ad0115588c764ff3987d7aac12648f1faf891dc3
Raja Bilal Saleem <rja.bilal@gmail.com>
2018-02-28 13:21:27 +1100

    Update Launch.py

--- Launch.py
+++ Launch.py
@@ -1,4 +1,4 @@
 #!usr/bin/env python
 import subprocess
 import os
-subprocess.call(['/home/ec2-kali/jenkins_slave/workspace/zap/zap.sh','-daemon'],stdout=open(os.devnull,'w'))
+subprocess.call(['/home/ec2-kali/jenkins_slave/workspace/zap/zap.sh'],stdout=open(os.devnull,'w'))
