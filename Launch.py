01e9f59a6e83402175e0622c95591fa7ccd946a7
Raja Bilal Saleem <rja.bilal@gmail.com>
2018-03-05 14:52:16 +1100

    Update Launch.py

--- Launch.py
+++ Launch.py
@@ -3,5 +3,5 @@ import subprocess
 import os
 import sys
 api = '-config api.key='+sys.argv[1]
-subprocess.call(['/home/ec2-kali/jenkins_slave/workspace/zap/zap.sh',api,'-daemon'])
+subprocess.call(['/home/ec2-kali/jenkins_slave/workspace/zap/zap.sh','-daemon'])
 
